821453fd01df76e4fdd96b579c2447418df01ecc
MuratALT <murat.altindal5@etu.univ-lorraine.fr>
2024-02-12 13:59:33 +0100

    feat(home, about): finishing home and about functionnalities

---
+++ src/app/tab/tab-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { TabPage } from './tab.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabPage,
+    children: [
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('../home/home.module').then((m) => m.HomePageModule),
+      },
+      {
+        path: 'about',
+        loadChildren: () =>
+          import('../about/about.module').then((m) => m.AboutPageModule),
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabPageRoutingModule {}
